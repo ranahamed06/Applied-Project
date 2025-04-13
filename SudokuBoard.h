593147e27ec9dc702b9193f5142b58218fd9d321
ranahamed06 <rana_alaa@aucegypt.edu>
2025-04-13 17:31:53 +0200

    Update SudokuBoard.h

--- SudokuBoard.h
+++ SudokuBoard.h
@@ -1,21 +1,23 @@
 #ifndef SUDOKUBOARD_H
 #define SUDOKUBOARD_H
 
+
 #include <iostream>
 #include "BacktrackTreeNode.h"
 
 class SudokuBoard {
     static const int SIZE = 9;  // Size of the board (9x9)
     int board[SIZE][SIZE];      // The Sudoku grid
+    bool solveRecursive(BacktrackTreeNode* node); //new
 public:
     SudokuBoard();  // Constructor to initialize the board
-    SudokuBoard(const SudokuBoard& other); 
+    SudokuBoard(const SudokuBoard& other); //new
     void setCell(int row, int col, int value);  // Declaration of the setCell function
     void printBoard();  // Display the current board
     bool isValidMove(int row, int col, int num);  // Validate a move
     bool findEmptyCell(int &row, int &col);
     bool solveSudoku();
-    bool solveWithTree(); 
+    bool solveWithTree(); //new
     bool isComplete();  // Check if the board is complete (no empty cells)
     void makeMove(int row, int col, int num);  // Place a number in a 
     // Clears the board
@@ -26,6 +28,8 @@ public:
     
     // Add a getter for a specific cell to allow for checking the puzzle state during random generation
     int getCell(int row, int col) const;
+    
+    
 };
 
 #endif
