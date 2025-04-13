af83e7d8c01afee53e579598f06277f69bb706e9
malaktaha2022 <malaktaha@aucegypt.edu>
2025-04-13 15:27:11 +0200

    created tree.cpp & tree.h

---
+++ BacktrackTree.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Malak Abdelhalim on 13/04/2025.
+//
+
+#include "BacktrackTree.h"
---
+++ BacktrackTree.h
@@ -0,0 +1,8 @@
+//
+// Created by Malak Abdelhalim on 13/04/2025.
+//
+
+#ifndef BACKTRACKTREE_H
+#define BACKTRACKTREE_H
+
+#endif //BACKTRACKTREE_H
