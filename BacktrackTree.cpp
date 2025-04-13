8c713dc9824dc68c795b168d4396977e7bc30c38
malaktaha2022 <malaktaha@aucegypt.edu>
2025-04-13 15:31:07 +0200

    started tree.cpp implementation

--- BacktrackTree.cpp
+++ BacktrackTree.cpp
@@ -3,3 +3,15 @@
 //
 
 #include "BacktrackTree.h"
+#include "SudokuBoard.h"
+
+#include <iostream> // Only used for debugging/logging purposes
+
+BacktrackTreeNode::BacktrackTreeNode(SudokuBoard* state, BacktrackTreeNode* parentNode)
+    : boardState(nullptr), parent(parentNode), children(nullptr), numChildren(0), capacity(2) {
+
+    // Deep copy of SudokuBoard (you need to implement a copy constructor in SudokuBoard)
+    boardState = new SudokuBoard(*state);
+
+    children = new BacktrackTreeNode*[capacity];
+}
